ce09a707c520eac94635bd59e7d84b65bb5e229d
yukihamu1206 <iida.y@tam-tam.co.jp>
2020-08-28 17:42:01 +0900

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sam build --use-container
-
-sam deploy --stack-name sam-app-sample \
-    --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM
+sam deploy --guided \
+           --stack-name sam-app-sample \
+           --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM
