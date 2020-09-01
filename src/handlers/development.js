b7e36a465321fc81a053c90f5c9a12104451d4d1
yukihamu1206 <iida.y@tam-tam.co.jp>
2020-09-01 16:58:41 +0900

    template.yml戻す

--- src/handlers/development.js
+++ src/handlers/development.js
@@ -1,4 +1,4 @@
-exports.development.handler = async (event) => {
+exports.handler = async (event) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda! this is development'),
