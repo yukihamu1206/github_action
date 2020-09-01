794e572816a6253f601406bd7bfeffe3f7ad2f2d
yukihamu1206 <iida.y@tam-tam.co.jp>
2020-09-01 16:54:01 +0900

    ハンドラー名

--- src/handlers/development.js
+++ src/handlers/development.js
@@ -1,4 +1,4 @@
-exports.handler = async (event) => {
+exports.development.handler = async (event) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda! this is development'),
