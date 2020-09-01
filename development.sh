4a34705ca26144408eb7ac9e4ef8edd810681656
yukihamu1206 <iida.y@tam-tam.co.jp>
2020-09-01 14:51:38 +0900

    development.sh

--- development.sh
+++ development.sh
@@ -2,11 +2,9 @@
 
 sam build --use-container
 sam package \
-    --output-template-file packaged.yaml \
     --s3-bucket githubactionsiida
 
 sam deploy \
-    --template-file packaged.yaml \
     --stack-name sam-development \
     --capabilities CAPABILITY_IAM \
     --parameter-overrides Env=development
