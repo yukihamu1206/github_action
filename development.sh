99d13dc6346c522329bef7a03e387efcdbbb750e
yukihamu1206 <iida.y@tam-tam.co.jp>
2020-09-02 13:57:37 +0900

    development.sh

--- development.sh
+++ development.sh
@@ -2,4 +2,4 @@ sam build --template development.yml
 sam deploy \
      --stack-name sam-development \
      --template-file development.yml \
-     --capabilities CAPABILITY_IAM
+     --s3-bucket githubactionsiida
