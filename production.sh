02cc1c8da4ac07ef339203b1bed585406a610832
yukihamu1206 <iida.y@tam-tam.co.jp>
2020-09-02 12:17:24 +0900

    Fix:production.shのコマンド修正

--- production.sh
+++ production.sh
@@ -1,12 +1,2 @@
-#!/bin/bash
-
-sam build --use-container
-sam package \
-    --output-template-file packaged.yaml \
-    --s3-bucket githubactionsiida
-
-sam deploy \
-    --template-file packaged.yaml \
-    --stack-name sam-production \
-    --capabilities CAPABILITY_IAM \
-    --parameter-overrides Env=production
+sam build 
+sam deploy --guided
