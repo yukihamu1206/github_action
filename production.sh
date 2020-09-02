da2aa283b70d6680f51ba18878a72125da97a96e
yukihamu1206 <iida.y@tam-tam.co.jp>
2020-09-02 14:15:41 +0900

    productionファイル修正

--- production.sh
+++ production.sh
@@ -2,4 +2,5 @@ sam build --template production.yml
 sam deploy \
      --stack-name sam-production \
      --template-file production.yml \
-     --s3-bucket githubactionsiida
+     --s3-bucket githubactionsiida \
+     --capabilities CAPABILITY_IAM
