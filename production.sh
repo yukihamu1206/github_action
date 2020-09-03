eeed319219d0248ae4a70ef137a7140ea56d9190
yukihamu1206 <iida.y@tam-tam.co.jp>
2020-09-03 15:26:18 +0900

    production.sh --template-file削除

--- production.sh
+++ production.sh
@@ -1,6 +1,5 @@
 sam build --template production.yml
 sam deploy \
      --stack-name sam-production \
-     --template-file production.yml \
      --s3-bucket githubactionsiida \
      --capabilities CAPABILITY_IAM
